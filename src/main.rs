f709f69aaf099c186d76550a943df844a4c09521
LetsMelon <domi.m@outlook.com>
2024-04-10 11:20:55 +0200

    use wrapper struct for wasm handled memory

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::collections::HashMap;
+use std::sync::Arc;
 
 use anyhow::{Context, Result};
+use glue::WasmMemory;
+use tokio::sync::Mutex;
 use wasmtime::{Config, Engine, Instance, Linker, Module, Store};
 use wasmtime_wasi::{WasiCtxBuilder, WasiP1Ctx};
 
@@ -35,19 +38,25 @@ async fn main() -> Result<()> {
 
     let instance = linker.instantiate_async(&mut store, &module).await?;
 
-    let fct_dealloc = instance.get_typed_func::<(i32, i32), ()>(&mut store, "dealloc")?;
+    let instance = Arc::new(instance);
+    let store = Arc::new(Mutex::new(store));
 
     {
-        let (secret_ptr, secret_len) =
-            copy_slice(b"It's a Secret to Everybody", &mut store, &instance).await?;
-        let (signature_ptr, signature_len) = copy_slice(
-            &hex_literal::hex!("757107ea0eb2509fc211221cce984b8a37570b6d7586c22c46f4379c8b043e17"),
-            &mut store,
-            &instance,
+        let secret = WasmMemory::new(
+            b"It's a Secret to Everybody",
+            instance.clone(),
+            store.clone(),
         )
         .await?;
-        let (payload_ptr, payload_len) =
-            copy_slice(b"Hello, World!", &mut store, &instance).await?;
+
+        let signature = WasmMemory::new(
+            &hex_literal::hex!("757107ea0eb2509fc211221cce984b8a37570b6d7586c22c46f4379c8b043e17"),
+            instance.clone(),
+            store.clone(),
+        )
+        .await?;
+
+        let payload = WasmMemory::new(b"Hello World!", instance.clone(), store.clone()).await?;
 
         let map = {
             let mut map = HashMap::new();
@@ -57,72 +66,53 @@ async fn main() -> Result<()> {
             map
         };
         let serialized_map = postcard::to_allocvec(&map).unwrap();
-        let (map_ptr, map_len) = copy_slice(&serialized_map, &mut store, &instance).await?;
+        let hashmap = WasmMemory::new(&serialized_map, instance.clone(), store.clone()).await?;
 
-        let fct_verify = instance.get_typed_func::<(i32, i32, i32, i32, i32, i32, i32, i32), i32>(
-            &mut store, "verify",
-        )?;
-        let result = fct_verify
-            .call_async(
-                &mut store,
-                (
-                    secret_ptr as i32,
-                    secret_len as i32,
-                    signature_ptr as i32,
-                    signature_len as i32,
-                    payload_ptr as i32,
-                    payload_len as i32,
-                    map_ptr as i32,
-                    map_len as i32,
-                ),
-            )
-            .await?;
-        dbg!(result);
-
-        fct_dealloc
-            .call_async(&mut store, (secret_ptr, secret_len as i32))
-            .await?;
-        fct_dealloc
-            .call_async(&mut store, (signature_ptr, signature_len as i32))
-            .await?;
-        fct_dealloc
-            .call_async(&mut store, (payload_ptr, payload_len as i32))
-            .await?;
-        fct_dealloc
-            .call_async(&mut store, (map_ptr, map_len as i32))
-            .await?;
+        let verfiy_result = verify(
+            secret,
+            payload,
+            signature,
+            hashmap,
+            instance.clone(),
+            store.clone(),
+        )
+        .await?;
+        dbg!(verfiy_result);
     }
-
-    // let wasm_saved = WasmMemory::new(
-    //     b"Hello World!",
-    //     Arc::new(instance),
-    //     Arc::new(Mutex::new(store)),
-    // )
-    // .await?;
-    // drop(wasm_saved);
 
     server_handle.await??;
 
     Ok(())
 }
 
-async fn copy_slice(
-    data: &[u8],
-    mut store: &mut Store<WasiP1Ctx>,
-    instance: &Instance,
-) -> anyhow::Result<(i32, usize)> {
-    let memory = instance
-        .get_memory(&mut store, "memory")
-        .context("expected memory not found")?;
+async fn verify(
+    secret: WasmMemory,
+    payload: WasmMemory,
+    signature: WasmMemory,
+    hashmap: WasmMemory,
+    instance: Arc<Instance>,
+    store: Arc<Mutex<Store<WasiP1Ctx>>>,
+) -> anyhow::Result<i32> {
+    let fct_verify = instance.get_typed_func::<(i32, i32, i32, i32, i32, i32, i32, i32), i32>(
+        &mut *store.lock().await,
+        "verify",
+    )?;
 
-    let fct_alloc = instance.get_typed_func::<i32, i32>(&mut store, "alloc")?;
+    let result = fct_verify
+        .call_async(
+            &mut *store.lock().await,
+            (
+                secret.ptr(),
+                secret.len() as i32,
+                signature.ptr(),
+                signature.len() as i32,
+                payload.ptr(),
+                payload.len() as i32,
+                hashmap.ptr(),
+                hashmap.len() as i32,
+            ),
+        )
+        .await?;
 
-    let ptr = fct_alloc.call_async(&mut store, data.len() as i32).await?;
-
-    unsafe {
-        let raw = memory.data_ptr(&mut store).offset(ptr as isize);
-        raw.copy_from(data.as_ptr(), data.len());
-    }
-
-    Ok((ptr, data.len()))
+    Ok(result)
 }
