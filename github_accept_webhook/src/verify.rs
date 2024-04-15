9c668e3035bf3dbfec0279a84213f5194ef8d9e2
LetsMelon <domi.m@outlook.com>
2024-04-15 11:42:44 +0200

    add simple test to check verify method

--- github_accept_webhook/src/verify.rs
+++ github_accept_webhook/src/verify.rs
@@ -10,3 +10,15 @@ pub fn verify(secret: &[u8], signature: &[u8], payload: &[u8]) -> Result<()> {
 
     Ok(())
 }
+
+#[test]
+fn github_demo() {
+    // https://docs.github.com/en/webhooks/using-webhooks/validating-webhook-deliveries
+
+    verify(
+        b"It's a Secret to Everybody",
+        &hex::decode("757107ea0eb2509fc211221cce984b8a37570b6d7586c22c46f4379c8b043e17").unwrap(),
+        b"Hello, World!",
+    )
+    .unwrap();
+}
